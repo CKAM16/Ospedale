daf42c60e9073fe64c51e47dfa7c31228b99559a
Keren <pkvega@uninorte.edu.co>
2026-05-23 21:25:56 -0500

    implemente en la clase LoginController los metodos login y logout usando repsonse

--- src/packagee/LoginController.java
+++ src/packagee/LoginController.java
@@ -9,5 +9,17 @@ package packagee;
  * @author paaoo
  */
 public class LoginController {
-    
+    public Response login(String username, String password) {
+        for (User u : DataStore.usuarios) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+                return new Response("SUCCESS", u.getRole());
+            }
+        }
+        return new Response("ERROR", "Usuario o contraseña incorrectos");
+    }
+
+    public Response logout() {
+        return new Response("SUCCESS", "Sesión cerrada correctamente");
+    }
 }
+
