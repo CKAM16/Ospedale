307f4ad15f72ded797765db4813bb3e6678b903e
Keren <pkvega@uninorte.edu.co>
2026-05-23 20:54:37 -0500

    Cree clase response para manejar la respuesta de los controladores

---
+++ src/packagee/Response.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package packagee;
+
+/**
+ *
+ * @author paaoo
+ */
+public class Response {
+    private String status; 
+    private String message;
+
+    public Response(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() { return status; }
+    public String getMessage() { return message; }
+}
